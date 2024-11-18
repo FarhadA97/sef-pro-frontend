b94957e191bb3d4d6ba3cda98278ed55813c7656
FarhadA97 <afarhad863@gmail.com>
2024-11-18 22:47:13 +0500

    Create home page scaffolding

---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,8 @@
+import { Home } from "@/sections/home";
+
+export default function HomePage() {
+	return (
+		<Home />
+	);
+}
+  
---
+++ src/sections/home/index.tsx
@@ -0,0 +1,7 @@
+export const Home = () => {
+  return (
+	<div className="overflow-hidden">
+		Home Page
+	</div>
+  );
+};
