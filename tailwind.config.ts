1a1b8010f72d62e1027a53d2fec783e78c3fc580
FarhadA97 <afarhad863@gmail.com>
2024-11-18 22:40:02 +0500

    Add necessary tailwind configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,14 @@
 import type { Config } from "tailwindcss";
+import tailwindAnimation from 'tailwindcss-animate'
+
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/layouts/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/sections/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
@@ -14,6 +18,6 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [tailwindAnimation]
 };
 export default config;
