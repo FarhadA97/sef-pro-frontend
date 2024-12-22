bd0cb2de567392f2b285301038201ffb68a31589
FarhadA97 <afarhad863@gmail.com>
2024-12-22 17:07:25 +0500

    UI fixes

--- src/sections/shop/index.tsx
+++ src/sections/shop/index.tsx
@@ -105,7 +105,7 @@ const ProductSection = ({
   return (
     products && (
       <div>
-        <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="mt-5 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
           {products.map((item) => (
             <ProductCard categoryId={categoryId} containerStyle="border relative" key={item.id} product={item} />
           ))}
